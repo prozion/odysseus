dcb5e9bd6db628a4c66b66a44ddfa27d2def4f36
Denis Shirshov <denis.shirshov@gmail.com>
2018-12-10 22:39:39 +0300

    54

--- deploy/system.rkt
+++ deploy/system.rkt
@@ -11,13 +11,14 @@
       ((regexp-match #rx"^c:.*" curdir)
         (let* ((user (getenv "userprofile"))
               (ms (get-matches "C:\\\\Users\\\\(.+)" user))
+              (ms (and (not-empty? ms) (first ms)))
               (user (if (> (length ms) 1) (second ms) #f)))
           (cond
-            ((equal? user "User") 'ab-24)
+            ((equal? user "User") 'inwin-win7)
             (else 'unknown-win))))
       ((regexp-match #rx"^/*" curdir)
         (let ((hostname (string-trim (read-file "/etc/hostname"))))
           (cond
-            ((equal? hostname "xpolaris") 'xpolaris)
+            ((equal? hostname "xpolaris") 'digitalocean)
             (else 'other-linux))))
       (else #f))))
--- report/widgets.rkt
+++ report/widgets.rkt
@@ -64,83 +64,84 @@
         (step (/f svg-width days-length))
         (users-list empty)
         )
-    (str
-        ; ticks
-        (for/fold
-          ((res ""))
-          ((tick (range 0 (inc days-length))))
-          (let* ((aday (+ min-start-time tick))
-                (adate (days->date aday))
-                (full-adate (days->date aday #:year #t))
-                (is-leap-year? (leap-year? (->number (third (split full-adate ".")))))
-                (adate-day (->number (first (split adate "."))))
-                (adate-month (second (split adate ".")))
-                (adate-month-abbr (nth months (->number adate-month)))
-                (first-day? (equal? 1 adate-day))
-                (adate (->string adate-day))
-                (canvas-height (- svg-height 50))
-                (y1 20))
-            (str
-              res "\n"
-              (line 'stroke "grey" 'stroke-width "1" 'opacity 0.5 'x1 (* step tick) 'y1 y1 'x2 (* step tick) 'y2 (+ y1 canvas-height)) "\n"
-              (when (holiday? (days->date aday #:year #t))
-                (rect 'x (* step tick) 'y y1 'width step 'height canvas-height 'class "holiday-bg"))
-              "\n"
-              ; today
-              (when (equal? (current-date) full-adate)
-                (rect 'x (* step tick) 'y y1 'width step 'height canvas-height 'class "today-bg"))
-              (when first-day? (text (@ 'x (+ (* 0.5 step) (* step tick)) 'y 12 'class "labels") adate-month-abbr))
-              (when
-                (or (equal? tick-dates 'all)
-                    (indexof? (map ->string '(1 3 5 7 10 12 15 17 20 23 25 27 29)) adate))
-                (text
-                  (@ 'x (+ (* 0.5 step) (* step tick)) 'y 33 'class "labels")
-                  adate))
-              "\n"
-            )))
-        ; bars
-        (for/fold
-          ((res ""))
-          ((job jobs) (i (in-naturals)))
-          (let* (
-                (user ($ user job))
-                (user (and user (car (split user ","))))
-                (_ (set! users-list (if (indexof? users-list user) users-list (pushr users-list user))))
-                (color (if user (list-ref colors (indexof users-list user)) (list-ref colors 0)))
-                (color (if (empty? (filter true? users-list)) (list-ref colors 1) color))
-                (color (if (equal? ($ status job) "done") "#bbb" color)) ; (color/shadow (color/desaturate color -55) 15)
-                (start-date (get-start-time job jobs))
-                (end-date (get-end-time job jobs))
-                (start (- (date->days start-date) min-start-time))
-                (end (- (date->days end-date) min-start-time))
-                (x (* step start))
-                (dx (* step end))
-                (width (- dx x))
-                (y (* track-h i))
-                (min_order (apply min (map (λ (x) ($ _order x)) jobs)))
-                (first-after (get-first-after job jobs))
-                (y-first-after (if first-after (* track-h (- first-after min_order)) #f))
-                (h (- track-h 5))
+    (g
+      (str
+          ; ticks
+          (for/fold
+            ((res ""))
+            ((tick (range 0 (inc days-length))))
+            (let* ((aday (+ min-start-time tick))
+                  (adate (days->date aday))
+                  (full-adate (days->date aday #:year #t))
+                  (is-leap-year? (leap-year? (->number (third (split full-adate ".")))))
+                  (adate-day (->number (first (split adate "."))))
+                  (adate-month (second (split adate ".")))
+                  (adate-month-abbr (nth months (->number adate-month)))
+                  (first-day? (equal? 1 adate-day))
+                  (adate (->string adate-day))
+                  (canvas-height (- svg-height 50))
+                  (y1 20))
+              (str
+                res "\n"
+                (line 'stroke "grey" 'stroke-width "1" 'opacity 0.5 'x1 (* step tick) 'y1 y1 'x2 (* step tick) 'y2 (+ y1 canvas-height)) "\n"
+                (when (holiday? (days->date aday #:year #t))
+                  (rect 'x (* step tick) 'y y1 'width step 'height canvas-height 'class "holiday-bg"))
+                "\n"
+                ; today
+                (when (equal? (current-date) full-adate)
+                  (rect 'x (* step tick) 'y y1 'width step 'height canvas-height 'class "today-bg"))
+                (when first-day? (text (@ 'x (+ (* 0.5 step) (* step tick)) 'y 12 'class "labels") adate-month-abbr))
+                (when
+                  (or (equal? tick-dates 'all)
+                      (indexof? (map ->string '(1 3 5 7 10 12 15 17 20 23 25 27 29)) adate))
+                  (text
+                    (@ 'x (+ (* 0.5 step) (* step tick)) 'y 33 'class "labels")
+                    adate))
+                "\n"
+              )))
+          ; bars
+          (for/fold
+            ((res ""))
+            ((job jobs) (i (in-naturals)))
+            (let* (
+                  (user ($ user job))
+                  (user (and user (car (split user ","))))
+                  (_ (set! users-list (if (indexof? users-list user) users-list (pushr users-list user))))
+                  (color (if user (list-ref colors (indexof users-list user)) (list-ref colors 0)))
+                  (color (if (empty? (filter true? users-list)) (list-ref colors 1) color))
+                  (color (if (equal? ($ status job) "done") "#bbb" color)) ; (color/shadow (color/desaturate color -55) 15)
+                  (start-date (get-start-time job jobs))
+                  (end-date (get-end-time job jobs))
+                  (start (- (date->days start-date) min-start-time))
+                  (end (- (date->days end-date) min-start-time))
+                  (x (* step start))
+                  (dx (* step end))
+                  (width (- dx x))
+                  (y (* track-h i))
+                  (min_order (apply min (map (λ (x) ($ _order x)) jobs)))
+                  (first-after (get-first-after job jobs))
+                  (y-first-after (if first-after (* track-h (- first-after min_order)) #f))
+                  (h (- track-h 5))
+                  )
+              (str
+                res "\n"
+                (g (@ 'transform (format "translate(0,~a)" y0))
+                  ; convergence line
+                  (when y-first-after
+                    (str (line 'x1 x 'y1 (+ y h) 'x2 x 'y2 y-first-after 'class "convergence_line") "\n"))
+                  ; bar
+                  (title ($ title job))
+                  (rect 'x x 'y y 'width width 'height h 'fill color 'opacity 0.6) "\n"
+                  ; (text (@ 'x (+ x 6) 'y (+ y 18) 'class "job_title_shadow") (namefy ($ id job))) "\n"
+                  (text (@ 'x (+ x 5) 'y (+ y (/ track-h 2.0) 2) 'class "job_title") (namefy ($ id job))) "\n"
                 )
+              )
+            ))
+          ; legend
+          (for/fold
+            ((res ""))
+            ((user users-list) (i (in-naturals)))
             (str
               res "\n"
-              (g (@ 'transform (format "translate(0,~a)" y0))
-                ; convergence line
-                (when y-first-after
-                  (str (line 'x1 x 'y1 (+ y h) 'x2 x 'y2 y-first-after 'class "convergence_line") "\n"))
-                ; bar
-                (title ($ title job))
-                (rect 'x x 'y y 'width width 'height h 'fill color 'opacity 0.6) "\n"
-                ; (text (@ 'x (+ x 6) 'y (+ y 18) 'class "job_title_shadow") (namefy ($ id job))) "\n"
-                (text (@ 'x (+ x 5) 'y (+ y (/ track-h 2.0) 2) 'class "job_title") (namefy ($ id job))) "\n"
-              )
-            )
-          ))
-        ; legend
-        (for/fold
-          ((res ""))
-          ((user users-list) (i (in-naturals)))
-          (str
-            res "\n"
-            (rect 'x (* i 100) 'y (- svg-height 20) 'width 20 'height 10 'fill (list-ref (rest colors) i))
-            (text (@ 'x (+ 25 (* i 100)) 'y (- svg-height 10)) user))))))
+              (rect 'x (* i 100) 'y (- svg-height 20) 'width 20 'height 10 'fill (list-ref (rest colors) i))
+              (text (@ 'x (+ 25 (* i 100)) 'y (- svg-height 10)) user)))))))
