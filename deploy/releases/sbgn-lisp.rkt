67f8275f100c6bcb5347b4bf1e7582251cd6f1ed
Denis Shirshov <denis.shirshov@gmail.com>
2019-04-12 21:37:05 +0300

    59.2

--- deploy/releases/sbgn-lisp.rkt
+++ deploy/releases/sbgn-lisp.rkt
@@ -2,11 +2,11 @@
 
 (require "../release.rkt")
 
-(extract-files
-  (list "lib")
-  ""
-  #:extract-to "c:/denis/denis_core/projects/sbgn-lisp"
-  #:exception-set (list
-    "lib/projects"
-    "lib/tests"
-))
+; (extract-files
+;   (list "lib")
+;   ""
+;   #:extract-to "c:/denis/denis_core/projects/sbgn-lisp"
+;   #:exception-set (list
+;     "lib/projects"
+;     "lib/tests"
+; ))
