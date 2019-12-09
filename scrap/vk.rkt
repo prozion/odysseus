e1f20d85988322ddf3f3cb68bb06193074aac1e9
Denis Shirshov <denis.shirshov@gmail.com>
2019-12-09 21:14:20 +0300

    76.2

--- scrap/vk.rkt
+++ scrap/vk.rkt
@@ -4,7 +4,7 @@
 (require "../../settings/APIs.rkt")
 (require net/url)
 (require json)
-(require browser/external)
+; (require browser/external)
 (require compatibility/defmacro)
 
 (define status-output (make-parameter #f))
@@ -17,16 +17,16 @@
 
 ; gets a new access_token if old is expired
 ; after launching this function it opens the browser, where you should accept the app permissions and then, on the redirected page, check access_token in the GET parameters in the browser address string
-(define (authenticate-through-browser)
-  (let* ((auth-url "https://oauth.vk.com/authorize")
-        (redirect_uri "")
-        (display "page")
-        (scope #b111111011110111011111)
-        (response_type "token")
-        (v "5.8")
-        (state "odysseus")
-        (request-string (url-with-parameters auth-url CID redirect_uri display scope response_type v state)))
-    (send-url request-string)))
+; (define (authenticate-through-browser)
+;   (let* ((auth-url "https://oauth.vk.com/authorize")
+;         (redirect_uri "")
+;         (display "page")
+;         (scope #b111111011110111011111)
+;         (response_type "token")
+;         (v "5.8")
+;         (state "odysseus")
+;         (request-string (url-with-parameters auth-url CID redirect_uri display scope response_type v state)))
+;     (send-url request-string)))
     ; (json->hash (get-url request-string))))
 
 (define-catch (dump-ids-to-file ids)
