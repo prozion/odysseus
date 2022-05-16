a31d96b4736332615142080112b2c79cbf5d9b4a
Sam Tobin-Hochstadt <samth@indiana.edu>
2022-05-16 16:33:25 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,9 @@
 #lang info
 
 (define collection "odysseus")
-(define deps '("sha" "sxml" "compatibility-lib" "rackunit"))
+(define deps '("base"
+               "htdp-lib"
+               "sha" "sxml" "compatibility-lib" "rackunit"))
 (define build-deps '("racket-doc" "scribble-lib"))
 (define scribblings '(("docs/odysseus.scrbl")))
 (define pkg-desc "Library with handy general-purpose functions and macro as well as specific ones. Uses as a midlayer between a bit nurdy vanilla Racket and practical problems met in the everyday life out on the street")
