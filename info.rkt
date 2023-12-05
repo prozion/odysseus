d7400d388637e22b07348a25af90ca08e6342ba1
Denis Shirshov <denis.shirshov@gmail.com>
2023-12-05 15:06:16 +0300

    125.1

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define collection "odysseus")
 (define deps '("compatibility-lib" "rackunit"))
 (define build-deps '("racket-doc" "scribble-lib"))
-(define scribblings '(("doc/odysseus.scrbl")))
+; (define scribblings '(("doc/odysseus.scrbl")))
 (define pkg-desc "Library with handy general-purpose functions and macro as well as specific ones. Uses as a midlayer between a bit nurdy vanilla Racket and practical problems met in the everyday life out on the street")
-(define version "2023.11")
+(define version "2023.12")
